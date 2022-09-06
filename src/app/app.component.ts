ee21e56c457f336672c99d217d9f46e4a2137b20
ManarYousry <77962166+ManarYousry@users.noreply.github.com>
2022-09-06 19:38:53 +0200

    fix translation issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BusyService } from './pages/dashboard/services/busy.service';
-import { PreloaderService } from './services/preloader.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import { PreloaderService } from './services/preloader.service';
 })
 export class AppComponent implements OnInit {
   title = 'saqeefah';
-constructor(private preloader: PreloaderService , private loader : BusyService){
+constructor(private loader : BusyService){
 this.loader.busy();
 }
 ngOnInit(): void {
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -14,8 +14,8 @@ export class DashboardComponent implements OnInit {
   user:string;
    email:string;
   constructor(private language:changeLanguageService,private router: Router, private auth: AuthService,private translate: TranslateService) {
-  this.translate.setDefaultLang('en');
-    // this.translate.use('ar')
+  //this.translate.setDefaultLang('en');
+     this.translate.use('ar')
 
   }
 
